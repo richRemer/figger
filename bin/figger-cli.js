701f0370b179a6ce49505e68be93f09badb6bba7
Richard Remer <rich@zingle.me>
2016-11-17 11:34:28 -0800

    new CLI front end, remove old files, release 3.0

--- bin/figger-cli.js
+++ bin/figger-cli.js
@@ -1,10 +1,12 @@
 #!/usr/bin/env node
 
-var figger = require(".."),
-    args = process.argv.slice(2),
-    envify;
+const PassThrough = require("stream").PassThrough;
+const figger = require("..");
+const token = figger.token;
 
-envify = Boolean(~args.indexOf("--envify"));
+var args = process.argv.slice(2),
+    envify = Boolean(~args.indexOf("--envify"));
+
 args = args.filter(arg => arg !== "--envify");
 
 if (args.length !== 1) {
@@ -12,9 +14,55 @@ if (args.length !== 1) {
     process.exit(1);
 }
 
-figger(envify ? figger.envify : figger.dump, args[0])
+figger.read(args[0])
+    .pipe(figger.evaluate())
+    .pipe(figger.clean())
+    .pipe(envify ? env() : str())
     .pipe(process.stdout)
-    .on("error", err => {
-        console.error(process.env.debug ? err.stack : err.message);
+    .on("error", e => console.error(process.env.debug ? e.stack : e.message));
+
+function str() {
+    return figger.stream.transform(function(chunk, done) {
+        if (chunk.type) this.push(String(chunk));
+        done();
     });
+}
+
+function env() {
+    var prev,
+        inquote = false;
+
+    return figger.stream.transform(function(chunk, done) {
+        switch (chunk.op || chunk.type) {
+            case token.ws:
+            case token.error:
+                // throw these out
+                break;
+            case token.comment:
+                // include leading whitespace with comment
+                if (prev && prev.type === token.ws) console.log(String(prev));
+                this.push(String(chunk));
+                break;
+            case token.quote:
+                this.push(String(chunk));
+                inquote = !inquote;
+                break;
+            case token.rawval:
+                if (~chunk.value.indexOf('"') && !inquote) this.push('"');
+                this.push(chunk.value.replace('"', '\\"'));
+                if (~chunk.value.indexOf('"') && !inquote) this.push('"');
+                break;
+            case token.eol:
+                this.push(String(chunk));
+                inquote = false;
+                break;
+            default:
+                if (chunk.type) this.push(String(chunk));
+                break;
+        }
+
+        prev = chunk;
+        done();
+    });
+}
 
