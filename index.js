3d3376709f7465889749cf6a0e299da232fd0377
Richard Remer <rich@zingle.me>
2016-11-17 11:02:25 -0800

    transform stream helper

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 module.exports.stream = {
+    transform: require("./lib/stream/transform"),
     readable: require("./lib/stream/readable")
 };
--- test/test-figger.js
+++ test/test-figger.js
@@ -1,6 +1,7 @@
 const expect = require("expect.js");
 const figger = require("..");
 const readable = figger.stream.readable;
+const transform = figger.stream.transform;
 
 describe("streams", () => {
     describe("readable", () => {
@@ -19,6 +20,31 @@ describe("streams", () => {
                 expect(results[1]).to.be("foo");
                 expect(results[2]).to.be(false);
                 expect(results[3]).to.be(obj);
+                done();
+            });
+        });
+    });
+
+    describe("transform", () => {
+        it("should make Transform stream using function", done => {
+            var input, T, output,
+                errored = false,
+                results = [];
+
+            input = readable(["Input", "Data"]);
+            T = transform(function(v, done) {
+                this.push(v.toLowerCase());
+                done();
+            });
+            output = input.pipe(T);
+
+            output.on("data", data => results.push(data));
+            output.on("error", err => errored = err);
+            output.on("end", () => {
+                expect(errored).to.be(false);
+                expect(results.length).to.be(2);
+                expect(results[0]).to.be("input");
+                expect(results[1]).to.be("data");
                 done();
             });
         });
