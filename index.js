2e98c437c729592302c5353f8cb9a32919074015
Richard Remer <rich@zingle.me>
2016-11-17 11:20:41 -0800

    add missing exports for stages and token

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
+module.exports.token = require("./lib/parse/token");
+module.exports.stage = {
+    assignments: require("./lib/stage/assignments"),
+    comments: require("./lib/stage/comments"),
+    dotincs: require("./lib/stage/dotincs"),
+    eols: require("./lib/stage/eols"),
+    errors: require("./lib/stage/errors"),
+    quotvals: require("./lib/stage/quotvals"),
+    rawvals: require("./lib/stage/rawvals"),
+    refs: require("./lib/stage/refs"),
+    trim: require("./lib/stage/trim"),
+}
+
 module.exports.stream = {
     transform: require("./lib/stream/transform"),
     readable: require("./lib/stream/readable")
